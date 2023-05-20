aa35994b3dc3b3cd6fb0f627089ccf330a4e3ce1
Shashkin <artyom.sh.s@yandex.ru>
2023-05-20 22:34:36 +0300

    done

---
+++ src/main/java/com/example/collectionsandsets/exception/EmployeeAlreadyAddedException.java
@@ -0,0 +1,23 @@
+package com.example.collectionsandsets.exception;
+
+public class EmployeeAlreadyAddedException extends RuntimeException {
+    public EmployeeAlreadyAddedException() {
+        super();
+    }
+
+    public EmployeeAlreadyAddedException(String message) {
+        super(message);
+    }
+
+    public EmployeeAlreadyAddedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmployeeAlreadyAddedException(Throwable cause) {
+        super(cause);
+    }
+
+    protected EmployeeAlreadyAddedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
